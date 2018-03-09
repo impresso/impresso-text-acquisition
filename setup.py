0abad469c730591d109a077eec90d41abba58f6a
Matteo Romanello <mromanello@tsf-492-wpa-3-145.epfl.ch>
2018-03-09 16:04:11 +0100

    organized the code more like a python module

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Config for Pypi."""
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = ""
+
+
+DESCRIPTION = """
+TODO
+"""
+
+setup(
+    name='text_acquisition',
+    author='Matteo Romanello',
+    author_email='matteo.romanello@epfl.ch',
+    url='https://github.com/impresso/impresso-text-acquisition',
+    version=VERSION,
+    packages=find_packages(),
+    package_data={
+        'text_importer': [
+            'data/*.*'
+        ]
+    },
+    long_description=DESCRIPTION,
+    install_requires=[
+        'bs4',
+        'docopt',
+        'pandas',
+        'dask[complete]',
+        'lxml',
+        'python-jsonschema-objects',
+    ]
+)
