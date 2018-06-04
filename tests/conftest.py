4fe923049210fc02b393a3f44817d9f743b4de9f
mromanello <matteo.romanello@gmail.com>
2018-06-04 17:15:34 +0200

    checked in the conftest file

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger()
--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,9 +1,24 @@
 import pkg_resources
 from impresso_commons.path import detect_issues
+from text_importer.importer import import_issues
 
 
-def test_import_issues():
-    inp_dir = pkg_resources.resource_filename('text_importer', 'data/out/')
+def test_olive_import_issues():
+    inp_dir = pkg_resources.resource_filename(
+        'text_importer',
+        'data/sample_data/'
+    )
     issues = detect_issues(inp_dir)
     assert issues is not None
     assert len(issues) > 0
+
+    result = import_issues(
+        issues,
+        "/Volumes/project_impresso/images/",
+        None,
+        pkg_resources.resource_filename('text_importer', 'data/out/'),
+        pkg_resources.resource_filename('text_importer', 'data/temp/'),
+        "olive",
+        True
+    )
+    print(result)
