2a3050b3a089f9643eade5ef6aa7a7e59411b1fc
mromanello <matteo.romanello@gmail.com>
2018-06-05 16:31:12 +0200

    now handling properly insertion of white space (in french texts)

--- tests/test_tokenization.py
+++ tests/test_tokenization.py
@@ -3,4 +3,6 @@ from text_importer.tokenization import insert_whitespace
 
 def test_insert_whitespace():
     assert insert_whitespace("Lausanne", ",", None, "fr") is False
-    # assert insert_whitespace(".", "01", "52", "fr") is False
+    assert insert_whitespace("(", "encore", None, "fr") is False
+    assert insert_whitespace(".", "01", "52", "fr") is False
+    assert insert_whitespace(",", "500", "000", "fr") is False
---
+++ text_importer/tokenization.py
@@ -0,0 +1,57 @@
+"""Tokenization rules for various languages0."""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+WHITESPACE_RULES = {
+    "fr": {
+        "punctuation_nows_before": [
+            ".",
+            ",",
+            ")",
+            "]",
+            "}",
+            "°",
+            "..."
+        ],
+        "punctuation_nows_after": ["(", "[", "{"],
+        "punctuation_nows_beforeafter": ["'", "-"],
+        "punctuation_ciffre": [".", ","]
+    }
+}
+
+
+def insert_whitespace(token, following_token, previous_token, language):
+    """Determine whether a whitespace should be inserted after a token."""
+    try:
+        wsrules = WHITESPACE_RULES[language]
+    except Exception:
+        pass
+        return
+
+    insert_ws = True
+
+    if (
+        token in wsrules["punctuation_nows_beforeafter"] or
+        following_token in wsrules["punctuation_nows_beforeafter"]
+    ):
+        insert_ws = False
+
+    elif following_token in wsrules["punctuation_nows_before"]:
+        insert_ws = False
+
+    elif token in wsrules["punctuation_nows_after"]:
+        insert_ws = False
+
+    elif token in wsrules["punctuation_ciffre"]:
+        if previous_token.isdigit() and following_token.isdigit():
+            return False
+        else:
+            return True
+
+    logger.debug(
+        f"Insert whitespace: curr={token}, follow={following_token}, \
+            prev={previous_token} ({insert_ws})"
+    )
+    return insert_ws
