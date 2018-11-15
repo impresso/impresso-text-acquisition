b971aa5864da4f5a8ae7339c7ef5edec4a3a0a9e
mromanello <matteo.romanello@gmail.com>
2018-11-15 09:24:03 +0100

    added test for BNL/Lux importer

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,6 +1,9 @@
 import pkg_resources
 from impresso_commons.path.path_fs import detect_issues
+
 from text_importer.importer import import_issues
+from text_importer.importers.lux import detect_issues as lux_detect_issues
+from text_importer.importers.lux import import_issues as lux_import_issues
 
 
 def test_olive_import_issues():
@@ -21,4 +24,16 @@ def test_olive_import_issues():
         "olive",
         True  # whether to parallelize or not
     )
+    print(result)
+
+
+def test_lux_importer():
+    inp_dir = pkg_resources.resource_filename(
+        'text_importer',
+        'data/sample_data/Luxembourg/'
+    )
+    out_dir = pkg_resources.resource_filename('text_importer', 'data/out/')
+    issues = lux_detect_issues(inp_dir)
+    assert issues is not None
+    result = lux_import_issues(issues, out_dir)
     print(result)
