aec68f7726a846794d9de5cc20639464639c3d1b
Matteo Romanello <matteo.romanello@gmail.com>
2019-04-05 15:43:49 +0200

    wip (dask memory issue)

--- text_importer/importers/lux/test_dask_memory.py
+++ text_importer/importers/lux/test_dask_memory.py
@@ -73,6 +73,85 @@ def process_pages(pages):
     return result
 
 
+def compress_pages(key, json_files, output_dir, prefix=""):
+    """Merges a set of JSON line files into a single compressed archive.
+
+    :param key: signature of the newspaper issue (e.g. GDL-1900)
+    :type key: str
+    :param json_files: input JSON line files
+    :type json_files: list
+    :param output_dir: directory where to write the output file
+    :type outp_dir: str
+    :return: a tuple with: sorting key [0] and path to serialized file [1].
+    :rytpe: tuple
+
+    .. note::
+
+        `sort_key` is expected to be the concatenation of newspaper ID and year
+        (e.g. GDL-1900).
+    """
+
+
+    newspaper, year, month, day, edition = key.split('-')
+    prefix_string = "" if prefix == "" else f"-{prefix}"
+    filename = f'{newspaper}-{year}-{month}-{day}-{edition}{prefix_string}.jsonl.bz2'
+    filepath = os.path.join(output_dir, filename)
+    print(f'Compressing {len(json_files)} JSON files into {filepath}')
+
+    with smart_open(filepath, 'wb') as fout:
+        writer = jsonlines.Writer(fout)
+
+        items = []
+        for issue, json_file in json_files:
+
+            with open(json_file, 'r') as inpf:
+                item = json.load(inpf)
+                items.append(item)
+
+        writer.write_all(items)
+        print(
+            f'Written {len(items)} docs from {json_file} to {filepath}'
+        )
+
+        writer.close()
+
+    return (key, filepath)
+
+
+def upload_pages(sort_key, filepath, bucket_name=None):
+    """Uploads a file to a given S3 bucket.
+    :param sort_key: the key used to group articles (e.g. "GDL-1900")
+    :type sort_key: str
+    :param filepath: path of the file to upload to S3
+    :type filepath: str
+    :param bucket_name: name of S3 bucket where to upload the file
+    :type bucket_name: str
+    :return: a tuple with [0] whether the upload was successful (boolean) and
+        [1] the path of the uploaded file (string)
+    .. note::
+        `sort_key` is expected to be the concatenation of newspaper ID and year
+        (e.g. GDL-1900).
+    """
+    # create connection with bucket
+    # copy contents to s3 key
+    newspaper, year, month, day, edition = sort_key.split('-')
+    key_name = "{}/{}/{}".format(
+        newspaper,
+        f'{newspaper}-{year}',
+        os.path.basename(filepath)
+    )
+    s3 = get_s3_resource()
+    try:
+        bucket = s3.Bucket(bucket_name)
+        bucket.upload_file(filepath, key_name)
+        print(f'Uploaded {filepath} to {key_name}')
+        return True, filepath
+    except Exception as e:
+        #logger.error(e)
+        #logger.error(f'The upload of {filepath} failed with error {e}')
+        print(f'The upload of {filepath} failed with error {e}')
+        return False, filepath
+
 @profile
 def main():
 
@@ -105,7 +184,7 @@ def main():
             .persist()
 
         progress(pages_bag)
-        print(f'Pages to process: {pages_bag.count().compute()}')
+        print(f'Pages to process: {pages_bag.count().compute()}\n')
 
         pages_bag = pages_bag\
             .repartition(1000)\
@@ -114,6 +193,20 @@ def main():
             .persist()
 
         progress(pages_bag)
+
+        print('Now compress and upload pages')
+        pages_bag = pages_bag.groupby(
+                lambda x: canonical_path(
+                    x[0], path_type='dir'
+                ).replace('/', '-')
+            )\
+            .starmap(compress_pages, prefix='pages', output_dir=out_dir)\
+            .starmap(upload_pages, bucket_name='original-canonical-data')\
+            .starmap.cleanup()\
+            .persist()
+
+        progress(pages_bag)
+
         print(gc.collect())
 
 main()
