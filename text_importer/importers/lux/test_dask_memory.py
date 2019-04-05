e74e1dd1180845062c22c3220920d84b2cd74a91
Matteo Romanello <matteo.romanello@gmail.com>
2019-04-05 14:26:55 +0200

    updated test_dask_memory.py

--- text_importer/importers/lux/test_dask_memory.py
+++ text_importer/importers/lux/test_dask_memory.py
@@ -11,6 +11,7 @@ from dask import bag as db
 from dask.distributed import Client, progress
 from impresso_commons.path.path_fs import canonical_path
 from impresso_commons.text.rebuilder import cleanup, upload
+from impresso_commons.utils import chunk
 from impresso_commons.utils.s3 import get_s3_resource
 from smart_open import smart_open
 
@@ -21,7 +22,6 @@ from text_importer.importers.lux.core import (compress_issues, issue2pages,
                                               serialize_page, upload_issues)
 from text_importer.importers.lux.detect import \
     detect_issues as lux_detect_issues
-
 
 
 def serialize_pages(pages, output_dir=None):
@@ -82,31 +82,37 @@ def main():
     out_dir = "/scratch/matteo/impresso-canonical/BNL/"
     s3_bucket = 'original-canonical-data'
     issues = lux_detect_issues(input_dir)
-    issue_bag = db.from_sequence(issues[:1000])
-    issue_bag =  issue_bag.filter(lambda i: i.journal == 'courriergdl')\
-            .map(mets2issue)\
-            .filter(lambda i: i is not None)\
+    selected_issues = db.from_sequence(issues)\
+        .filter(lambda i: i.journal == 'courriergdl').compute()
+    chunks = chunk(selected_issues, 1000)
+
+    for chunk_n, chunk_of_issues in enumerate(chunks):
+
+        print(f'Processing chunk {chunk_n}/{len(chunk)}')
+
+        issue_bag =  issue_bag.from_sequence(chunk_of_issues)\
+                .map(mets2issue)\
+                .filter(lambda i: i is not None)\
+                .persist()
+
+
+        progress(issue_bag)
+
+        pages_bag = issue_bag\
+            .map(issue2pages)\
+            .flatten()\
             .persist()
 
+        progress(pages_bag)
+        print(f'Pages to process: {pages_bag.count().compute()}')
 
-    progress(issue_bag)
+        pages_bag = pages_bag\
+            .repartition(1000)\
+            .map_partitions(process_pages)\
+            .map_partitions(serialize_pages, output_dir=out_dir)\
+            .persist()
 
-    pages_bag = issue_bag\
-        .map(issue2pages)\
-        .flatten()\
-        .persist()
-
-    progress(pages_bag)
-    print(f'Pages to process: {pages_bag.count().compute()}')
-
-    pages_bag = pages_bag\
-        .repartition(1000)\
-        .map_partitions(process_pages)\
-        .map_partitions(serialize_pages, output_dir=out_dir)\
-        .persist()
-
-    progress(pages_bag)
-    print(gc.collect())
+        progress(pages_bag)
+        print(gc.collect())
 
 main()
-import ipdb; ipdb.set_trace()
