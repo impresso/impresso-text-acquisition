f5500d8f3d88433e514ae56d50f12ba7f1c2b322
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2019-08-05 15:48:03 +0200

    Add olive_detect_issues function

---
+++ text_importer/importers/olive/detect.py
@@ -0,0 +1,36 @@
+from collections import namedtuple
+from typing import List
+import json
+from impresso_commons.path.path_fs import IssueDir, detect_issues, select_issues
+
+from text_importer.utils import get_access_right
+
+OliveIssueDir = namedtuple(
+        "IssueDirectory", [
+                'journal',
+                'date',
+                'edition',
+                'path',
+                'rights'
+                ]
+        )
+
+
+def dir2olivedir(issue_dir: IssueDir, access_rights: dict) -> OliveIssueDir:
+    ar = get_access_right(issue_dir.journal, issue_dir.date, access_rights)
+    return OliveIssueDir(issue_dir.journal, issue_dir.date, issue_dir.edition, issue_dir.path, rights=ar)
+
+
+def olive_detect_issues(base_dir: str, access_rights: str, journal_filter: set = None, exclude: bool = False) \
+        -> List[OliveIssueDir]:
+    with open(access_rights, 'r') as f:
+        access_rights_dict = json.load(f)
+    issues = detect_issues(base_dir, journal_filter=journal_filter, exclude=exclude)
+    
+    return [dir2olivedir(x, access_rights_dict) for x in issues]
+
+
+def olive_select_issues(base_dir: str, config: dict, access_rights_dict: dict) -> List[OliveIssueDir]:
+    issues = select_issues(config, base_dir)
+    
+    return [dir2olivedir(x, access_rights_dict) for x in issues]
