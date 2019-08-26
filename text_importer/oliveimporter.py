b72f56cc1978f58c75066a3a9f140def2f8d95a6
Matteo Romanello <matteo.romanello@gmail.com>
2019-08-26 17:22:29 +0200

    reorganised individual importers into the scripts folder

--- text_importer/oliveimporter.py
+++
@@ -1,6 +0,0 @@
-from text_importer.importers.olive.classes import OliveNewspaperIssue
-from text_importer.importers.olive.detect import olive_select_issues, olive_detect_issues
-from text_importer.importers import generic_importer
-
-if __name__ == '__main__':
-    generic_importer.main(OliveNewspaperIssue, olive_detect_issues, olive_select_issues)
