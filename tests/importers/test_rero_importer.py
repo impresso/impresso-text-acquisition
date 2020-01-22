a5fd1b283e7d38500a150022c4f7df41239e0f91
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-01-22 12:49:09 +0100

    Bug fix for rero/mets_alto on image coordinates. Added 'c' property in issue json and 'iiif_link' for images

--- tests/importers/test_rero_importer.py
+++ tests/importers/test_rero_importer.py
@@ -1,53 +1,54 @@
-import pkg_resources
-from glob import glob
-
-from text_importer.importers.core import import_issues
-from text_importer.importers.rero.detect import detect_issues
-from text_importer.importers.rero.classes import ReroNewspaperIssue
-from text_importer.importers import CONTENTITEM_TYPE_IMAGE
 import bz2
 import json
 import logging
 import os
+from glob import glob
+
+import pkg_resources
+
+from text_importer.importers import CONTENTITEM_TYPE_IMAGE
+from text_importer.importers.core import import_issues
+from text_importer.importers.rero.classes import ReroNewspaperIssue
+from text_importer.importers.rero.detect import detect_issues
 
 logger = logging.getLogger(__name__)
 
 
-# def test_import_issues():
-#     """Test the Olive XML importer with sample data."""
-#
-#     inp_dir = pkg_resources.resource_filename(
-#         'text_importer',
-#         'data/sample_data/RERO2/'
-#     )
-#
-#     access_rights_file = pkg_resources.resource_filename(
-#         'text_importer',
-#         'data/sample_data/RERO2/rero2_access_rights.json'
-#     )
-#
-#     issues = detect_issues(
-#         base_dir=inp_dir,
-#         access_rights=access_rights_file
-#     )
-#     assert issues is not None
-#     assert len(issues) > 0
-#
-#     import_issues(
-#         issues,
-#         out_dir=pkg_resources.resource_filename('text_importer', 'data/out/'),
-#         s3_bucket=None,
-#         issue_class=ReroNewspaperIssue,
-#         temp_dir=None,
-#         image_dirs=None,
-#         chunk_size=None
-#     )
+def test_import_issues():
+    """Test the Olive XML importer with sample data."""
+    
+    inp_dir = pkg_resources.resource_filename(
+            'text_importer',
+            'data/sample_data/RERO2/'
+            )
+    
+    access_rights_file = pkg_resources.resource_filename(
+            'text_importer',
+            'data/sample_data/RERO2/rero2_access_rights.json'
+            )
+    
+    issues = detect_issues(
+            base_dir=inp_dir,
+            access_rights=access_rights_file
+            )
+    assert issues is not None
+    assert len(issues) > 0
+    
+    import_issues(
+            issues,
+            out_dir=pkg_resources.resource_filename('text_importer', 'data/out/'),
+            s3_bucket=None,
+            issue_class=ReroNewspaperIssue,
+            temp_dir=None,
+            image_dirs=None,
+            chunk_size=None
+            )
 
 
 def check_image_coordinates(issue_data):
     items = issue_data['i']
     images = [i for i in items if i['m']['tp'] == CONTENTITEM_TYPE_IMAGE]
-    return all('c' in data['m'] for data in images)
+    return len(images) == 0 or all('c' in data['m'] and len(data['m']['c']) == 4 for data in images)
 
 
 def test_image_coordinates():
