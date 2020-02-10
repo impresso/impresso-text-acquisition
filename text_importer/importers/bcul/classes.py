41254cab6ffe2631984dff8c3236b87b8df9f83a
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-02-10 17:36:42 +0100

    Add image retrieval from page data

--- text_importer/importers/bcul/classes.py
+++ text_importer/importers/bcul/classes.py
@@ -1,8 +1,12 @@
-from text_importer.importers.classes import NewspaperIssue, NewspaperPage
-from text_importer.importers.bcul.helpers import find_mit_file, get_page_number
+import codecs
 import logging
 import os
+from time import strftime
+
 from bs4 import BeautifulSoup
+
+from text_importer.importers.bcul.helpers import find_mit_file, get_page_number, get_image_coords
+from text_importer.importers.classes import NewspaperIssue, NewspaperPage
 
 logger = logging.getLogger(__name__)
 
@@ -13,11 +17,40 @@ class BCULNewspaperPage(NewspaperPage):
         super().__init__(_id, number)
         self.path = page_path
     
+    @property
+    def xml(self) -> BeautifulSoup:
+        if self.path.endswith("bz2"):
+            with codecs.open(self.path, encoding="bz2") as f:
+                xml = BeautifulSoup(f, "xml")
+        elif self.path.endswith("xml"):
+            with open(self.path) as f:
+                xml = BeautifulSoup(f, "xml")
+        return xml
+    
+    @property
+    def ci_id(self) -> str:
+        """
+        Return the content item ID of the page.
+
+        Given that BCUL data do not entail article-level segmentation,
+        each page is considered as a content item. Thus, to mint the content
+        item ID we take the canonical page ID and simply replace the "p"
+        prefix with "i".
+
+        :return: str Content item id
+        """
+        split = self.id.split('-')
+        split[-1] = split[-1].replace('p', 'i')
+        return "-".join(split)
+    
     def add_issue(self, issue: NewspaperIssue):
         self.issue = issue
     
+    def get_image_divs(self):
+        return self.xml.findAll("block", {"blockType": "Picture"})
+    
     def parse(self):
-        print("NTM")
+        pass
 
 
 class BCULNewspaperIssue(NewspaperIssue):
@@ -32,14 +65,31 @@ class BCULNewspaperIssue(NewspaperIssue):
         if not (self.is_json or self.is_xml):
             logger.error("Mit file {} is not JSON nor XML".format(self.mit_file))
             raise ValueError
+        
+        self.content_items = []
+        
+        self._find_pages()
+        self._find_content_items()
+        
+        self.issue_data = {
+            'id': self.id,
+            'cdt': strftime("%Y-%m-%d %H:%M:%S"),
+            'i': self.content_items,
+            'ar': self.rights,
+            'pp': [p.id for p in self.pages]
+            }
     
     def _find_pages_xml(self):
+        """ Finds the pages when the format is XML
+        
+        :return:
+        """
         # List all files and get the filenames from the MIT file
         files = os.listdir(self.path)
         
         with open(self.mit_file) as f:
             mit = BeautifulSoup(f, "xml")
-            
+        
         pages = sorted([os.path.basename(x.get('xml')) for x in mit.findAll("image")])
         for p in pages:
             found = False
@@ -56,6 +106,10 @@ class BCULNewspaperIssue(NewspaperIssue):
                 logger.error("Page {} not found in {}".format(p, self.path))
     
     def _find_pages_json(self):
+        """ Finds the pages when the format is JSON
+        
+        :return:
+        """
         # Get all exif files
         files = [os.path.join(self.path, x) for x in os.listdir(self.path) if os.path.splitext(x)[0].endswith("exif")]
         for f in files:
@@ -68,7 +122,29 @@ class BCULNewspaperIssue(NewspaperIssue):
             self.pages.append(page)
     
     def _find_pages(self):
+        """ Finds the pages of the current issue
+        Since BCUL has two different formats for the issue level, we need to take both into account
+        
+        :return:
+        """
         if self.is_json:
             self._find_pages_json()
         elif self.is_xml:
             self._find_pages_xml()
+    
+    def _find_content_items(self):
+        for n, page in enumerate(sorted(self.pages, key=lambda x: x.id)):
+            ci_id = self.id + '-i' + str(n + 1).zfill(4)
+            ci = {
+                'm': {
+                    'id': ci_id,
+                    'pp': [n + 1],
+                    'tp': 'page',
+                    }
+                }
+            self.content_items.append(ci)
+    
+    def _find_images(self):
+        # Get all images
+        image_divs = [(x, p.number, get_image_coords(x)) for p in self.pages for x in p.get_image_divs()]
+        # TODO: continue this
--- text_importer/importers/bcul/helpers.py
+++ text_importer/importers/bcul/helpers.py
@@ -1,16 +1,27 @@
 import os
-from datetime import datetime
+from datetime import datetime, date
 import json
+from typing import Tuple
 
 
-def parse_info(mit_filename):
-    basename, _ = os.path.splitext(os.path.basename(mit_filename.split('/')[-1]))
+def parse_info(mit_filename: str) -> Tuple[date, bytes]:
+    """ Given the Mit filename, parses the Journal name and the date
+    
+    :param str mit_filename:
+    :return:
+    """
+    basename = os.path.splitext(os.path.basename(mit_filename.split('/')[-1]))[0]
     split = basename.split('_')
     year, month, day = int(split[-4]), int(split[-3]), int(split[-2])
     return datetime(year, month, day).date(), split[0]
 
 
-def find_mit_file(_dir):
+def find_mit_file(_dir: str) -> str:
+    """ Given a directory, searches for a file ending with `mit`
+    
+    :param str _dir: Directory to look into
+    :return:
+    """
     mit_file = None
     for f in os.listdir(_dir):
         if os.path.splitext(os.path.basename(f))[0].endswith("mit"):
@@ -19,7 +30,7 @@ def find_mit_file(_dir):
     return mit_file
 
 
-def get_page_number(exif_file):
+def get_page_number(exif_file: str) -> int:
     """ Given an exif file (for the JSON flavour of BCUL) looks for the page number inside the EXIF file
     
     :param str exif_file:
@@ -33,3 +44,11 @@ def get_page_number(exif_file):
             return page_no
     except Exception as e:
         raise ValueError("Could not get page number from {}".format(exif_file))
+
+
+def get_image_coords(image_div):
+    rect_div = image_div.find("rect")
+    if rect_div is None:
+        return None
+    b, l, r, t = rect_div.get('b'), rect_div.get('l'), rect_div.get('r'), rect_div.get('t')
+    return [b, l, r, t]
