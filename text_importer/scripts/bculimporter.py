30df24f3066c0ee3e4c8fa4a827402f84ed8ce93
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-02-11 15:19:43 +0100

    Add BCUL script

---
+++ text_importer/scripts/bculimporter.py
@@ -0,0 +1,6 @@
+from text_importer.importers.bcul.classes import BCULNewspaperIssue
+from text_importer.importers.bcul.detect import detect_issues as bcul_detect_issues, select_issues as bcul_select_issues
+from text_importer.importers import generic_importer
+
+if __name__ == '__main__':
+    generic_importer.main(BCULNewspaperIssue, bcul_detect_issues, bcul_select_issues)
