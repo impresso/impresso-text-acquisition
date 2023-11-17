324d30d1c56e2b863f011a76eb742d9bd769b8f8
piconti <pauline.conti@epfl.ch>
2023-11-17 11:22:53 +0100

    Add page iiif uris and issue manifest uris

--- text_importer/importers/bcul/classes.py
+++ text_importer/importers/bcul/classes.py
@@ -21,22 +21,23 @@ BCUL_CI_TRANSLATION = {
     BCUL_IMAGE_TYPE: CONTENTITEM_TYPE_IMAGE,
     BCUL_TABLE_TYPE: CONTENTITEM_TYPE_TABLE
 }
-IIIF_BASE_URL = "https://scriptorium.bcu-lausanne.ch/api/iiif"
-IIIF_IMG_BASE_URL = f"{IIIF_BASE_URL}-img"
+IIIF_PRES_BASE_URI = "https://scriptorium.bcu-lausanne.ch/api/iiif"
+IIIF_IMG_BASE_URI = f"{IIIF_PRES_BASE_URI}-img"
 IIIF_SUFFIX = 'info.json'
 IIIF_MANIFEST_SUFFIX = 'manifest'
 
 
 class BCULNewspaperPage(NewspaperPage):
     
-    def __init__(self, _id: str, number: int, page_path: str):
+    def __init__(self, _id: str, number: int, page_path: str, iiif_uri: str):
         super().__init__(_id, number)
         self.path = page_path
-        
+        self.iiif_base_uri = iiif_uri
         self.page_data = {
             'id': _id,
             'cdt': strftime("%Y-%m-%d %H:%M:%S"),
-            'r': []  # here go the page regions
+            'r': [],  # here go the page regions
+            'iiif_img_base_uri': iiif_uri,
             }
     
     @property
@@ -96,14 +97,66 @@ class BCULNewspaperIssue(NewspaperIssue):
         
         self._find_pages()
         self._find_content_items()
+
+        # issue manifest, identifier is directory name
+        self.iiif_manifest = os.path.join(IIIF_PRES_BASE_URI, 
+                                          self.path.split('/')[-1], 
+                                          IIIF_MANIFEST_SUFFIX)
         
         self.issue_data = {
             'id': self.id,
             'cdt': strftime("%Y-%m-%d %H:%M:%S"),
             'i': self.content_items,
             'ar': self.rights,
-            'pp': [p.id for p in self.pages]
+            'pp': [p.id for p in self.pages],
+            'iiif_manifest_uri': self.iiif_manifest
             }
+
+    def _get_iiif_link_json(self, page_path: str) -> str:
+        """Return iiif image base uri in case `mit` file is in JSON.
+
+        In this case, the page identifier is simply the page XML file's name.
+
+        Args:
+            page_path (str): Path to the page XML file the content item is on.
+
+        Returns:
+            str: IIIF image base uri (without suffix).
+        """
+        # when mit file is a JSON, each page file's name is the iiif identifier
+        page_identifier = os.path.basename(page_path).split('.')[0]
+        return os.path.join(IIIF_IMG_BASE_URI, page_identifier)
+
+    def _get_iiif_link_xml(self, page_number: int) -> str | None:
+        """Return iiif image base uri in case `mit` file is in XML.
+
+        In this case, the iiif URI to images needs to be fetched from the iiif
+        presentation API, in the issue's manifest.
+
+        Args:
+            page_number (int): Page number for which to fetch the iiif URI.
+
+        Returns:
+            str | None: IIIF image base uri (no suffix) or None if request to 
+                presentation API failed.
+        """
+        try:
+            response = requests.get(self.iiif_manifest)
+            if response.status_code == 200:
+                iiif = (
+                    response.json()['sequences'][0]['canvases'][page_number]
+                        ['images'][0]['resource']['@id']
+                )
+                # replace suffix mapping to page's image by information json.
+                return '/'.join(iiif.split('/')[:-4])
+            else:
+                logger.error(f"Got {response.status_code} response while "
+                             f"querying the IIIF API for {self.id}.")
+        except Exception as e:
+            logger.error(f'Error while querying IIIF API for {self.id}: {e}.')
+        logger.warning(f"No iiif link stored for content item {self.id}.")
+        # Exception or wrong HTTP response, so no iiif link to return
+        return None
     
     def _find_pages_xml(self):
         """ Finds the pages when the format is XML
@@ -125,7 +178,8 @@ class BCULNewspaperIssue(NewspaperIssue):
                     page_path = os.path.join(self.path, f)
                     page_no = int(f.split('.')[0].split('_')[-1])
                     page_id = "{}-p{}".format(self.id, str(page_no).zfill(4))
-                    page = BCULNewspaperPage(page_id, page_no, page_path)
+                    page_iiif = self._get_iiif_link_xml(page_no)
+                    page = BCULNewspaperPage(page_id, page_no, page_path, page_iiif)
                     self.pages.append(page)
                     found = True
             if not found:
@@ -144,7 +198,8 @@ class BCULNewspaperIssue(NewspaperIssue):
             page_path = os.path.join(self.path, "{}.xml".format(file_id))
             page_no = get_page_number(f)
             page_id = "{}-p{}".format(self.id, str(page_no).zfill(4))
-            page = BCULNewspaperPage(page_id, page_no, page_path)
+            page_iiif = self._get_iiif_link_json(page_path)
+            page = BCULNewspaperPage(page_id, page_no, page_path, page_iiif)
             self.pages.append(page)
     
     def _find_pages(self):
@@ -158,55 +213,6 @@ class BCULNewspaperIssue(NewspaperIssue):
         elif self.is_xml:
             self._find_pages_xml()
 
-    def _get_iiif_link_json(self, page_path: str) -> str:
-        """Return iiif uri in case `mit` file is in JSON.
-
-        In this case, the page identifier is simply the page XML file's name.
-
-        Args:
-            page_path (str): Path to the page XML file the content item is on.
-
-        Returns:
-            str: IIIF image uri to the image information.
-        """
-        # when mit file is a JSON, each page file's name is the iiif identifier
-        page_identifier = os.path.basename(page_path).split('.')[0]
-        return os.path.join(IIIF_IMG_BASE_URL, page_identifier, IIIF_SUFFIX)
-
-    def _get_iiif_link_xml(self, page_number: int) -> str | None:
-        """Return iiif uri in case `mit` file is in XML.
-
-        In this case, the iiif URI to images needs to be fetched from the iiif
-        presentation API, in the issue's manifest.
-
-        Args:
-            page_number (int): Page number for which to fetch the iiif URI.
-
-        Returns:
-            str | None: IIIF image uri to the image information or None if 
-                request to presentation API failed.
-        """
-        identifier = self.path.split('/')[-1]
-        try:
-            response = requests.get(
-                os.path.join(IIIF_BASE_URL, identifier, IIIF_MANIFEST_SUFFIX)
-            )
-            if response.status_code == 200:
-                iiif = (
-                    response.json()['sequences'][0]['canvases'][page_number]
-                        ['images'][0]['resource']['@id']
-                )
-                # replace suffix mapping to page's image by information json.
-                return '/'.join(iiif.split('/')[:-4]+[IIIF_SUFFIX])
-            else:
-                logger.error(f"Got {response.status_code} response while "
-                             f"querying the IIIF API for {self.id}.")
-        except Exception as e:
-            logger.error(f'Error while querying IIIF API for {self.id}: {e}.')
-        logger.warning(f"No iiif link stored for content item {self.id}.")
-        # Exception or wrong HTTP response, so no iiif link to return
-        return None
-    
     def _find_content_items(self):
         # First add the pages as content items
         for n, page in enumerate(sorted(self.pages, key=lambda x: x.number)):
@@ -237,10 +243,7 @@ class BCULNewspaperIssue(NewspaperIssue):
 
                 # Content item is an image TODO check this works with samples that have images
                 if ci['m']['tp'] == CONTENTITEM_TYPE_IMAGE:
-                    if self.is_json:
-                        ci['m']['iiif_link'] = self._get_iiif_link_json(p.path)
-                    elif self.is_xml: 
-                        ci['m']['iiif_link'] = self._get_iiif_link_xml(p.number)
+                    ci['m']['iiif_link'] = os.path.join(p.iiif_base_uri, IIIF_SUFFIX)
                     ci['c'] = coords
 
                 self.content_items.append(ci)
