1a725d904f0be550097808b6fe531a9db1bce76f
piconti <pauline.conti@epfl.ch>
2024-03-04 18:50:17 +0100

    include the access rights and alias json

--- text_importer/importers/bcul/detect.py
+++ text_importer/importers/bcul/detect.py
@@ -1,25 +1,21 @@
 """This module contains helper functions to find BCUL OCR data to import.
 """
+
 import logging
 import os
+import json
 from collections import namedtuple
 from typing import Optional
 
 from dask import bag as db
 from impresso_commons.path.path_fs import _apply_datefilter
 
-from text_importer.importers.bcul.helpers import parse_info, find_mit_file
+from text_importer.importers.bcul.helpers import parse_date, find_mit_file
 
 logger = logging.getLogger(__name__)
 
 BculIssueDir = namedtuple(
-    "IssueDirectory", [
-        'journal',
-        'date',
-        'edition',
-        'path',
-        'rights'
-    ]
+    "IssueDirectory", ["journal", "date", "edition", "path", "rights", "mit_file_type"]
 )
 """A light-weight data structure to represent a newspaper issue.
 
@@ -38,6 +34,7 @@ Args:
     edition (str): Edition of the newspaper issue ('a', 'b', 'c', etc.).
     path (str): Path to the directory containing the issue's OCR data.
     rights (str): Access rights on the data (open, closed, etc.).
+    rights (str): Type of mit file for this issue (json or xml).
 
 >>> from datetime import date
 >>> i = BculIssueDir(
@@ -45,12 +42,13 @@ Args:
     date=datetime.date(1762, 12, 07), 
     edition='a', 
     path='./BCUL/46165', 
-    rights='open_public'
+    rights='open_public',
+    mit_file_type:'json'
 )
 """
 
 
-def dir2issue(path: str, access_rights: dict) -> Optional[BculIssueDir]:
+def dir2issue(path: str, journal_info: dict[str, str]) -> Optional[BculIssueDir]:
     """Create a `BculIssueDir` object from a directory.
 
     Note:
@@ -65,12 +63,28 @@ def dir2issue(path: str, access_rights: dict) -> Optional[BculIssueDir]:
     """
     mit_file = find_mit_file(path)
     if mit_file is None:
-        logger.error("Could not find MIT file in {}".format(path))
+        logger.error("Could not find MIT file in %s", path)
         return None
-    date, journal = parse_info(mit_file)
-    
-    return BculIssueDir(journal=journal, date=date, edition="a",
-                        path=path, rights="open_public")
+
+    if not mit_file.endswith(journal_info["file_type"]):
+        logger.warning(
+            "Found mit file %s does not correspond to mit file type %s",
+            path,
+            journal_info["file_type"],
+        )
+        # override the mit file type if the extension of the file found does not match
+        journal_info["file_type"] = mit_file.split(".")[-1]
+
+    date = parse_date(mit_file)
+
+    return BculIssueDir(
+        journal=journal_info["alias"],
+        date=date,
+        edition="a",
+        path=path,
+        rights=journal_info["access_right"],
+        mit_file_type=journal_info["file_type"],
+    )
 
 
 def detect_issues(base_dir: str, access_rights: str) -> list[BculIssueDir]:
@@ -81,26 +95,30 @@ def detect_issues(base_dir: str, access_rights: str) -> list[BculIssueDir]:
 
     Args:
         base_dir (str): Path to the base directory of newspaper data.
-        access_rights (str): Path to ``access_rights.json`` file.
+        access_rights (str): Path to `access_rights_and_aliases.json` file.
 
     Returns:
         list[BculIssueDir]: List of `BCULIssueDir` instances, to be imported.
     """
-    
+    with open(access_rights, "rb") as f:
+        ar_and_alias = json.load(f)
+
     dir_path, dirs, files = next(os.walk(base_dir))
-    
-    journal_dirs = [os.path.join(dir_path, _dir) 
-                    for _dir in dirs 
-                    if _dir not in ['OLD', 'wrong_BCUL']]
+
+    journal_dirs = [
+        os.path.join(dir_path, _dir)
+        for _dir in dirs
+        if _dir not in ["OLD", "wrong_BCUL"] and _dir in ar_and_alias
+    ]
 
     issue_dirs = []
     for journal in journal_dirs:
-        logger.info(f"Detecting issues for {journal}.")
+        logger.info("Detecting issues for %s.", journal)
         for dir_path, dirs, files in os.walk(journal):
-            if len(files)>1 and 'solr' not in dir_path:
-                issue_dirs.append(dir_path)
+            if len(files) > 1 and "solr" not in dir_path:
+                issue_dirs.append(dir2issue(dir_path, ar_and_alias[journal]))
 
-    return [dir2issue(_dir, None) for _dir in issue_dirs]
+    return issue_dirs
 
 
 def select_issues(
@@ -116,39 +134,42 @@ def select_issues(
     Args:
         base_dir (str): Path to the base directory of newspaper data.
         config (dict): Config dictionary for filtering.
-        access_rights (str): Not used for this imported, but argument is kept 
+        access_rights (str): Not used for this imported, but argument is kept
             for uniformity.
 
     Returns:
         Optional[list[BculIssueDir]]: List of `BculIssueDir` to import.
     """
-    
+
     # read filters from json configuration (see config.example.json)
     try:
         filter_dict = config["newspapers"]
         exclude_list = config["exclude_newspapers"]
         year_flag = config["year_only"]
-    
+
     except KeyError:
-        logger.critical(f"The key [newspapers|exclude_newspapers|year_only] "
-                        "is missing in the config file.")
+        logger.critical(
+            f"The key [newspapers|exclude_newspapers|year_only] "
+            "is missing in the config file."
+        )
         return
-    
+
     issues = detect_issues(base_dir, access_rights)
     issue_bag = db.from_sequence(issues)
-    selected_issues = (
-        issue_bag.filter(
-            lambda i: (
-                len(filter_dict) == 0 or i.journal in filter_dict.keys()
-            ) and i.journal not in exclude_list
-        ).compute()
-    )
-    
+    selected_issues = issue_bag.filter(
+        lambda i: (len(filter_dict) == 0 or i.journal in filter_dict.keys())
+        and i.journal not in exclude_list
+    ).compute()
+
     exclude_flag = False if not exclude_list else True
-    filtered_issues = _apply_datefilter(
-        filter_dict, selected_issues, year_only=year_flag
-    ) if not exclude_flag else selected_issues
-    logger.info(f"{len(filtered_issues)} newspaper issues remained "
-                f"after applying filter: {filtered_issues}")
-    
+    filtered_issues = (
+        _apply_datefilter(filter_dict, selected_issues, year_only=year_flag)
+        if not exclude_flag
+        else selected_issues
+    )
+    logger.info(
+        f"{len(filtered_issues)} newspaper issues remained "
+        f"after applying filter: {filtered_issues}"
+    )
+
     return filtered_issues
--- text_importer/importers/bcul/helpers.py
+++ text_importer/importers/bcul/helpers.py
@@ -1,4 +1,5 @@
 """Helper functions to parse BCUL OCR files."""
+
 import os
 from datetime import datetime, date
 import json
@@ -9,22 +10,25 @@ from typing import Any
 def replace_alias(current_alias: str, journal_name: str) -> str:
     # TODO replace by a csv file
     # some title aliases repeat or don't match our format restrictions
-    if 'GAV' in current_alias:
-        return 'GAVi'
-    elif 'feuille' in current_alias :
-        return 'feuilleP' if journal_name in "Feuille_d'avis_de_Payerne" else 'feuillePMA'
-    elif 'TO-SU-IL' in current_alias:
-        return 'TouSuIl'
-    elif 'Le_Phare_de_Nyon' in journal_name:
-        return 'PDN'
-    elif 'FAN' in current_alias and 'Le_Phare_du_' in journal_name:
-        return 'PDL'
-    elif 'VVS' in current_alias and 'Le_Veveysan_1' in journal_name:
-        return 'VVS1'
+    if "GAV" in current_alias:
+        return "GAVi"
+    elif "feuille" in current_alias:
+        return (
+            "feuilleP" if journal_name in "Feuille_d'avis_de_Payerne" else "feuillePMA"
+        )
+    elif "TO-SU-IL" in current_alias:
+        return "TouSuIl"
+    elif "Le_Phare_de_Nyon" in journal_name:
+        return "PDN"
+    elif "FAN" in current_alias and "Le_Phare_du_" in journal_name:
+        return "PDL"
+    elif "VVS" in current_alias and "Le_Veveysan_1" in journal_name:
+        return "VVS1"
     else:
         return current_alias
 
-def parse_info(mit_filename: str) -> tuple[date, str]:
+
+def parse_date(mit_filename: str) -> tuple[date, str]:
     """Given the Mit filename, parse the Journal name and the date.
 
     Args:
@@ -33,18 +37,19 @@ def parse_info(mit_filename: str) -> tuple[date, str]:
     Returns:
         Tuple[date, str]: Publication date of the issue, and journal name.
     """
-    split = mit_filename.split('/')
+    split = mit_filename.split("/")
     # extract date of publication
     year, month, day = int(split[-5]), int(split[-4]), int(split[-3])
-    #normalize the month & day values if they are out of range
+    # normalize the month & day values if they are out of range
     month = max(min(month, 12), 1)
     day = max(min(day, 31), 1)
 
     # extract journal name alias
-    basename = os.path.splitext(split[-1])[0]
-    journal_alias = replace_alias(basename.split('_')[0], split[-6])
-    
-    return datetime(year, month, day).date(), journal_alias
+    # basename = os.path.splitext(split[-1])[0]
+    # journal_alias = replace_alias(basename.split('_')[0], split[-6])
+
+    return datetime(year, month, day).date()  # , journal_alias
+
 
 def find_mit_file(_dir: str) -> str:
     """Given a directory, search for a file with a name ending with `mit`.
@@ -59,7 +64,7 @@ def find_mit_file(_dir: str) -> str:
     for f in os.listdir(_dir):
         if os.path.splitext(os.path.basename(f))[0].endswith("mit"):
             mit_file = os.path.join(_dir, f)
-    
+
     return mit_file
 
 
@@ -81,11 +86,12 @@ def get_page_number(exif_file: str) -> int:
     try:
         with open(exif_file) as f:
             exif = json.load(f)[0]
-            source = exif['SourceFile'].split('/')[-1]
-            page_no = int(os.path.splitext(source)[0].split('_')[-1])
+            source = exif["SourceFile"].split("/")[-1]
+            page_no = int(os.path.splitext(source)[0].split("_")[-1])
             return page_no
     except Exception as e:
         raise ValueError("Could not get page number from {}".format(exif_file))
+
 
 def find_page_file_in_dir(base_path: str, file_id: str) -> str | None:
     page_path = os.path.join(base_path, "{}.xml".format(file_id))
@@ -94,22 +100,28 @@ def find_page_file_in_dir(base_path: str, file_id: str) -> str | None:
         page_path = f"{page_path}.bz2"
         if not os.path.isfile(page_path):
             # logger.critical instead? if yes, add continue in classes line 258
-            raise Exception(f"The page file {page_path} couldn't be found."
-                            "Skipping this page, please verify input data.")
+            raise Exception(
+                f"The page file {page_path} couldn't be found."
+                "Skipping this page, please verify input data."
+            )
 
     return page_path
 
+
 def verify_issue_has_ocr_files(path: str) -> None:
     if not any([".xml" in f for f in os.listdir(path)]):
-        raise Exception(f"The issue's folder {path} does not contain any xml "
-                        "OCR files. Issue cannot be processed as a result.")
+        raise Exception(
+            f"The issue's folder {path} does not contain any xml "
+            "OCR files. Issue cannot be processed as a result."
+        )
+
 
 def get_div_coords(div: Tag) -> list[int]:
     """Extract the coordinates from the given element and format them for iiif.
 
-    In Abbyy format, the coordinates are denoted by the bottom, top (y-axis), 
+    In Abbyy format, the coordinates are denoted by the bottom, top (y-axis),
     left and right (x-axis) values.
-    But iiif coordinates should be formatted as `[x, y, width, height]`, where 
+    But iiif coordinates should be formatted as `[x, y, width, height]`, where
     (x,y) denotes the box's top left corner: (l, t). Thus they need conversion.
 
     Args:
@@ -120,8 +132,8 @@ def get_div_coords(div: Tag) -> list[int]:
     """
     if div is None:
         return None
-    b, l = int(div.get('b')), int(div.get('l'))
-    r, t = int(div.get('r')), int(div.get('t'))
+    b, l = int(div.get("b")), int(div.get("l"))
+    r, t = int(div.get("r")), int(div.get("t"))
     return [l, t, r - l, b - t]
 
 
@@ -150,8 +162,8 @@ def parse_textline(line: Tag) -> dict[str, list[Any]]:
     """
     line_ci = {"c": get_div_coords(line)}
     tokens = [parse_token(t) for t in line.findAll("charParams")]
-    
-    line_ci['t'] = tokens
+
+    line_ci["t"] = tokens
     return line_ci
 
 
@@ -166,16 +178,12 @@ def parse_textblock(block: Tag, page_ci_id: str) -> dict[str, Any]:
         dict[str, Any]: Parsed region object in canonical format.
     """
     coordinates = get_div_coords(block)
-    
+
     lines = [parse_textline(line) for line in block.findAll("line")]
     paragraph = {
         "c": coordinates,
         "l": lines,
     }
-    region = {
-        "c": coordinates,
-        "p": [paragraph],
-        "pOf": page_ci_id
-    }
+    region = {"c": coordinates, "p": [paragraph], "pOf": page_ci_id}
 
     return region
