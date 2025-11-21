bc7429bea647aa6b8b5bbe6e666408f7b762eccc
Coralie Banuls <banuls@iccluster028.iccluster.epfl.ch>
2025-11-21 11:43:46 +0100

    detect had changed but now is good

--- text_preparation/importers/bcul/detect.py
+++ text_preparation/importers/bcul/detect.py
@@ -49,7 +49,7 @@ Args:
 # issues that lead to HTTP response 404. Skipping them altogether.
 # These issues are often dublicates of issues for which the API works
 # In addition, it was found that some issues were listed with wrong dates.
-OLD_ALIASES_FILEPATH = "../data/sample_data/BCUL/access_rights_and_aliases.json"
+# OLD_ALIASES_FILEPATH = "../data/sample_data/BCUL/access_rights_and_aliases.json"
 ALIASES_FILEPATH = "/rcp-scratch/iccluster040_scratch/students/banuls/impresso-text-acquisition/text_preparation/data/sample_data/BCUL/bcul_aliases.json"
 FAULTY_ISSUES = [
     "127626",
