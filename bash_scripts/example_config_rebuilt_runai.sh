ff0897611a4c54d02955c29c4431a088a12f3a53
piconti <pauline.conti@epfl.ch>
2025-12-03 15:34:43 +0100

    update all the paths for rebuilt on runai

--- bash_scripts/example_config_rebuilt_runai.sh
+++ bash_scripts/example_config_rebuilt_runai.sh
@@ -11,31 +11,35 @@ export input_bucket='' # TODO fill in
 
 export $USER_NAME = 'piconti'
 
-export pvc_path="/home/$USER_NAME/dhlab-data/data/$USER_NAME-data"
-export scratch_pvc_path = "/home/$USER_NAME/scratch/piconti"
+# old pvc where the code was before to the "data" pvc -> change to the actual pvc path of /rcp-scratch
+#export pvc_path="/home/$USER_NAME/dhlab-data/data/$USER_NAME-data" 
+export pcv_path="/rcp-scratch/"
+export text_prep_in_pvc_path = "/${USER_NAME}/impresso/impresso-text-acquisition"
+# directory with all the outputs, logs, and configs
+export log_outputs_in_pvc_path = iccluster040_scratch/$USER_NAME/impresso/rebuilt_rcp
 
-# log file
+# log file goes in the dir for all rebuilt rcp outputs
 logfile_name="self_explanatory_logfilename.log" # TODO change
-touch $pvc_path/impresso-text-acquisition/text_preparation/data/logs/rebuilt_logs/$logfile_name
-export log_file="${pvc_path}/impresso-text-acquisition/text_preparation/data/logs/rebuilt_logs/${logfile_name}"
+touch $pvc_path/$log_outputs_in_pvc_path/logs/$logfile_name
+export log_file="${pcv_path}/${log_outputs_in_pvc_path}/logs/${logfile_name}"
 
 #format
-export format='passim' # 'solr' or 'passim'
+export format='solr' # 'solr' or 'passim'
 
 #output_dir
 #export output_dir="${pvc_path}/impresso-text-acquisition/text_preparation/data/rebuilt_out"
-export output_dir="${scratch_pvc_path}/rebuilt_output"
+export output_dir="${pcv_path}/${log_outputs_in_pvc_path}/out"
 
 #filter config
 filter_config_filename='chosen_or_created_config_file.json' # TODO change
-export filter_config="${pvc_path}/impresso-text-acquisition/text_preparation/config/rebuilt_config/${filter_config_filename}"
+export filter_config="${pvc_path}/$text_prep_in_pvc_path/config/${filter_config_filename}"
 
 #git repo
-export git_repo="${pvc_path}/impresso-text-acquisition"
+export git_repo=${text_prep_in_pvc_path}
 
 #temp dir
-mkdir -p $scratch_pvc_path/temp_rebuilt 
-export temp_dir="${scratch_pvc_path}/temp_rebuilt"
+mkdir -p ${pvc_path}/${log_outputs_in_pvc_path}/temp
+export temp_dir="${scratch_pvc_path}/temp"
 
-# path to the previous manifest
+# s3 path to the previous manifest
 export prev_manifest_path=""
